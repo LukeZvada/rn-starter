6d0cdbbdaedec3fa76b8f719396809ba8a655b52
LukeZvada <zvadamusic@gmail.com>
2021-03-05 00:06:14 -0800

    added navigation props destructuring as well as Buttons for the user to navigate

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+//instead of passing props as an argument you can pass only what you need to shorten you code
+//in this case all we needed from props is props.navigation.navigate
+//This is called destructuring 
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button 
+        onPress={() => navigation.navigate('Components')}
+        title="Go To Components Demo" 
+      />
+      <Button 
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
