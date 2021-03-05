60eca054bd6f52847534b7611f3a50334177074c
LukeZvada <zvadamusic@gmail.com>
2021-03-05 10:47:12 -0800

    added image score to ImageDetail props

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -3,19 +3,25 @@ import { Text, StyleSheet, View, Image } from "react-native";
 import ImageDetial from "../components/ImageDetail";
 
 const ImageScreen = () => {
+    //title, imgSource, and imgScore are props that can named anything
+    //we use these to pass to the ImageDetail using props as the arg
+    
     return (
         <View>
             <ImageDetial 
                 title="Forest" 
-                imageSource={require('../../assets/forest.jpg')} 
+                imageSource={require('../../assets/forest.jpg')}
+                imageScore='9'
             />
             <ImageDetial 
                 title="Beach" 
                 imageSource={require('../../assets/beach.jpg')} 
+                imageScore='7'
             />
             <ImageDetial 
                 title="Mountain" 
                 imageSource={require('../../assets/mountain.jpg')} 
+                imageScore='4'
             />
         </View>
     )
