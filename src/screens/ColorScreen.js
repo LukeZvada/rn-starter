d496ae64cfa7485581a32828107949e11a1d209f
LukeZvada <zvadamusic@gmail.com>
2021-03-05 11:45:00 -0800

    color generator working properly

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, FlatList } from "react-native";
 import { useState } from "react";
 
 
 const ColorScreen = () => {
-const [color, setColor] = useState(0)
+const [colors, setColors] = useState([])
 
     return (
         <View>
@@ -12,13 +12,25 @@ const [color, setColor] = useState(0)
             <Button 
                 title="Add a color" 
                 onPress={() => {
-
+                    setColors([...colors, randomRgb()])
             }} />
             <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
+            <FlatList 
+            //In this key extractor the rgb string generated is unique. So we dont need to pass a property name
+            keyExtractor={(item) => item}
+            data={colors} 
+            renderItem={({item}) => {
+                return (
+                    <View style={{ height: 100, width: 100, backgroundColor: item }} />
+                )
+            }}
+        />
         </View>
     )
 };
 
+
+//this is creating random rgb color numbers and returning a string rgb(243, 12, 239)
 const randomRgb = () => {
     const red = Math.floor(Math.random() * 256)
     const green = Math.floor(Math.random() * 256)
