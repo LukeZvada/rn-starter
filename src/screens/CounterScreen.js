59d26cfc9e1a38c02212e7817563795185f321f8
LukeZvada <zvadamusic@gmail.com>
2021-03-05 11:23:47 -0800

    added useState to increase and decrease counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
+import { useState } from "react";
 
 
 const CounterScreen = () => {
-    let counter = 0
+const [counter, setCounter] = useState(0)
 
     return (
         <View>
-            <Button title="Increase" onPress={() => {
-                counter++;
+            <Button 
+                title="Increase" 
+                onPress={() => {
+                    setCounter(counter + 1);
             }} />
-            <Button title="Decrease" onPress={() => {
-                counter--;
+            <Button 
+                title="Decrease" 
+                    onPress={() => {
+                    setCounter(counter - 1);
             }} />
             <Text>Current Count: {counter}</Text>
         </View>
