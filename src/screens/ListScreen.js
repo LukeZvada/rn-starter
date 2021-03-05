3d34e4c97261beb5ec7e2fefa6f36e41c5c85de3
LukeZvada <zvadamusic@gmail.com>
2021-03-04 23:31:13 -0800

    added horizontal styles and keyprop

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, FlatList } from 'react-native'
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>
+    const friends = [
+        {name: 'John'},
+        {name: 'Bill'},
+        {name: 'Ted'},
+        {name: 'Cyndi'},
+        {name: 'Fred'}
+    ]
+    return (
+        <FlatList 
+            //horizontal prop will change it from a column to a roll. Scroll left to right instead of up and down
+            horizontal
+            //hides the scroll bar while scrolling left to right 
+            showsHorizontalScrollIndicator={false}
+            //Use id in the future since it is unique. Similar to ReactJS key prop during NSS
+            keyExtractor={(friends) => friends.name}
+            data={friends} 
+            renderItem={({item}) => {
+                return <Text style={styles.textStyle}>{item.name}</Text>
+            }} 
+        />
+    )
 }
 
-const styles = StyleSheet.create({})
+//styling for the 
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50,
+        marginLeft: 20
+    }
+})
 
 export default ListScreen
